701c11ca92af161d300d28a94390955f3a366ff6
Manish Devrani <manishdevrani777@gmail.com>
2022-03-26 18:21:57 +0530

    feat-added login page

--- src/pages/video-listing/VideoListing.jsx
+++ src/pages/video-listing/VideoListing.jsx
@@ -2,18 +2,20 @@ import React from "react";
 import { Navbar } from "../../components";
 import { Chips } from "../../components";
 import { Thumbnail } from "../../components";
+import { Login } from "../login/Login";
 import "./VideoListing.css";
 
 const VideoListing = () => {
   return (
     <div className="video-listing">
       <Navbar />
-      <Chips />
+      <Login />
+      {/* <Chips />
       <div className="video-listing__container grid">
         {[...Array(10)].map((item) => {
           return <Thumbnail />;
         })}
-      </div>
+      </div> */}
     </div>
   );
 };
