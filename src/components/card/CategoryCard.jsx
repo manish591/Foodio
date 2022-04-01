7c9409a3ea24b47de8d8822af878c01c80d9b2f1
Manish Devrani <manishdevrani777@gmail.com>
2022-04-01 21:50:52 +0530

    feat-show product based on category selected in home page

--- src/components/card/CategoryCard.jsx
+++ src/components/card/CategoryCard.jsx
@@ -1,18 +1,23 @@
 import React from "react";
 
+import { useLocation, useNavigate } from "react-router-dom";
+
 const CategoryCard = ({ _id, categoryName }) => {
+  const navigate = useNavigate();
+
   return (
     <section
-      class="card card--text-over-media"
+      className="card card--text-over-media"
       style={{
         inlineSize: "100%",
         position: "relative",
         blockSize: "100%",
       }}
+      onClick={() => navigate("/explore", { state: categoryName })}
     >
-      <div class="card__content-above" style={{ bottom: "10%" }}>
+      <div className="card__content-above" style={{ bottom: "10%" }}>
         <h3
-          class="card__title"
+          className="card__title"
           style={{
             color: "var(--text2)",
             marginBlockEnd: "2rem",
@@ -21,7 +26,7 @@ const CategoryCard = ({ _id, categoryName }) => {
         >
           {categoryName}
         </h3>
-        <p class="card__author" style={{ color: "var(--text2)" }}>
+        <p className="card__author" style={{ color: "var(--text2)" }}>
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente,
           iusto voluptatem veniam magni similique voluptas mollitia neque
           excepturi! Recusandae, aliquid!
