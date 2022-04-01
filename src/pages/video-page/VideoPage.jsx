2f1fa12773202cebe4758cbd98ad672d6e3af465
Manish Devrani <manishdevrani777@gmail.com>
2022-04-01 23:47:37 +0530

    style-fix spacing issue in single video page in mobile view

--- src/pages/video-page/VideoPage.jsx
+++ src/pages/video-page/VideoPage.jsx
@@ -62,10 +62,7 @@ const VideoPage = () => {
               </p>
             </section>
           </div>
-          <div
-            className="vd-desc__right vp-actions"
-            style={{ marginInlineStart: "auto" }}
-          >
+          <div className="vd-desc__right vp-actions">
             <section className="vp-actions__top flex">
               {isAlreadyInDatabaseVideo(state.library.likedVideos, videoId) ? (
                 <button
@@ -121,17 +118,22 @@ const VideoPage = () => {
             <section className="vp-actions__bottom grid">
               <div
                 className="flex"
-                style={{ alignItems: "center", opacity: "0.6" }}
+                style={{
+                  alignItems: "center",
+                  opacity: "0.6",
+                  alignItems: "center",
+                  gap: "0.2rem",
+                }}
               >
                 <span className="material-icons-outlined">visibility</span>
-                <p>6856 Views</p>
+                <p style={{ whiteSpace: "nowrap" }}>6856 Views</p>
               </div>
               <div
                 className="flex"
                 style={{ alignItems: "center", opacity: "0.6", opacity: "0.6" }}
               >
                 <span className="material-icons-outlined">thumb_up</span>
-                <p>1016 Likes</p>
+                <p style={{ whiteSpace: "nowrap" }}>1016 Likes</p>
               </div>
               <div
                 className="flex"
@@ -140,14 +142,14 @@ const VideoPage = () => {
                 <span className="material-icons-outlined">
                   chat_bubble_outline
                 </span>
-                <p>75 Comments</p>
+                <p style={{ whiteSpace: "nowrap" }}>75 Comments</p>
               </div>
               <div
                 className="flex"
                 style={{ alignItems: "center", opacity: "0.6" }}
               >
                 <span className="material-icons-outlined">event</span>
-                <p>Dec 20, 2022</p>
+                <p style={{ whiteSpace: "nowrap" }}>Dec 20, 2022</p>
               </div>
             </section>
           </div>
