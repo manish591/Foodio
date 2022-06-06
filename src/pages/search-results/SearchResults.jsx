951e2c9a0f86e75e6fc656aa24630f60c2faab69
Manish Devrani <manishdevrani777@gmail.com>
2022-06-06 15:19:03 +0530

    added not found page if search results not found

--- src/pages/search-results/SearchResults.jsx
+++ src/pages/search-results/SearchResults.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import './SearchResults.css';
-import { useSearchParams } from 'react-router-dom';
+import { Link, useSearchParams } from 'react-router-dom';
 import { Thumbnail } from '../../components';
 import { useStateContext } from '../../hooks';
 
@@ -20,19 +20,28 @@ const SearchResults = () => {
         <h2 className="search-results__title fw-100">
           Showing Search Results for <strong>&quot;{searchQuery}</strong>&quot;
         </h2>
-        <section className="search-results__container grid">
-          {searchResultsList.map((item) => {
-            return (
-              <Thumbnail
-                key={item._id}
-                video={item}
-                page="Search"
-                setSelectedId={setSelectedId}
-                selectedId={selectedId}
-              />
-            );
-          })}
-        </section>
+        {searchResultsList.length < 1 ? (
+          <section className="search-result__not-found search-not-found">
+            <h1 className="search-not-found__title">No Videos found</h1>
+            <Link to="/explore" className="btn btn--contained-primary search-not-found__back">
+              Go Back
+            </Link>
+          </section>
+        ) : (
+          <section className="search-results__container grid">
+            {searchResultsList.map((item) => {
+              return (
+                <Thumbnail
+                  key={item._id}
+                  video={item}
+                  page="Search"
+                  setSelectedId={setSelectedId}
+                  selectedId={selectedId}
+                />
+              );
+            })}
+          </section>
+        )}
       </div>
     </div>
   );
